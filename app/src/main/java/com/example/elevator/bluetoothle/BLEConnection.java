3a4f8e6a5f1210771118ab01b79308892532991f
Gen <ydalite01@gmail.com>
2022-07-01 16:09:02 +0400

    test

--- app/src/main/java/com/example/elevator/bluetoothle/BLEConnection.java
+++ app/src/main/java/com/example/elevator/bluetoothle/BLEConnection.java
@@ -387,6 +387,7 @@ public class BLEConnection<IBluetoothGatt> implements BluetoothProfile {
                     command += target;
                     if(connect_cabine(item, command)) is_was_connected = true;
                 }
+                String test = "test";
             }
         } else {
             Log.d(TAG, "WAS CONNECTED TO FLOOR");
